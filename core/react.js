ab2a15c6673be3d52190c9c98f45d1be049458eb
freekong <jianghuaidai@gmail.com>
2024-01-14 13:23:12 +0800

    基于requestIdleCallback实现fiber架构

--- core/react.js
+++ core/react.js
@@ -17,22 +17,85 @@ function createTextNode(text) {
   }
 }
 
-function render(node, container) {
+let nextWorkofUnit = null
+
+function workLoop(deadline) {
   
-  const dom = node.type !== 'TEXT_ELEMENT' ? document.createElement(node.type) : document.createTextNode('')
- 
-  Object.keys(node.props).forEach(key => {
+  let shouldYield = false
+  while(!shouldYield && nextWorkofUnit) {
+    
+    nextWorkofUnit = performWorkOfUnit(nextWorkofUnit)
+    shouldYield = deadline.timeRemaining() < 1
+    
+  }
+
+  requestIdleCallback(workLoop)
+}
+
+function createDom(type) {
+  return type !== 'TEXT_ELEMENT' ? document.createElement(type) : document.createTextNode('')
+}
+
+function updateProps(fiber, dom) {
+  Object.keys(fiber.props).forEach(key => {
     if (key !== 'children') {
-      dom[key] = node.props[key]
+      dom[key] = fiber.props[key]
     }
   })
-  
-  node.props.children.forEach(item => {
-    render(item, dom)
-  })
-
-  container.append(dom)
 }
+
+function initChild(fiber) {
+  let prevChild = null
+  fiber.props.children.forEach((child, index) => {
+    const newFiber = {
+      type: child.type,
+      props: child.props,
+      parent: fiber,
+      child: null,
+      sibling: null
+    }
+    if (index === 0) {
+      fiber.child = newFiber
+    } else {
+      prevChild.sibling = newFiber
+    }
+    prevChild = newFiber
+  })
+}
+
+function performWorkOfUnit(fiber) {
+  // 创建dom
+  if (!fiber.dom) {
+    const dom = (fiber.dom = createDom(fiber.type))
+    fiber.parent.dom.append(dom)
+    // 处理props
+    updateProps(fiber, dom)
+  }
+  // 处理链式关系
+  initChild(fiber)
+  // 返回下一个任务
+  if (fiber.child) {
+    return fiber.child
+  }
+  if (fiber.sibling) {
+    return fiber.sibling
+  }
+
+  return fiber.parent?.sibling
+}
+
+function render(node, container) {
+
+  nextWorkofUnit = {
+    dom: container,
+    props: {
+      children: [node]
+    }
+  }
+  
+}
+
+requestIdleCallback(workLoop)
 
 export default {
   createElement,
